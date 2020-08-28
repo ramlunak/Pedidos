e872f9e8ce6f941f38dbd74f9b641f36e9441c00
Your Name <you@example.com>
2020-08-28 16:15:08 -0300

    header

--- Pedidos/Models/P_Categoria.cs
+++ Pedidos/Models/P_Categoria.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,14 @@ namespace Pedidos.Models
 {
     public class P_Categoria
     {
-        public int id { get; set; }      
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatorio")]
+        [DisplayName("Nome")]
         public string nombre { get; set; }     
         public int idCuenta { get; set; }
+
+        [DisplayName("Ativo")]
         public bool activo { get; set; } = true;
     }
 }

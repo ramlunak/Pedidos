8688e65b1e9450bd4ef123c08e930c2ccfe3b0fa
Your Name <you@example.com>
2020-10-11 11:39:11 -0300

    P_PedidoProductos

---
+++ Pedidos/Models/P_PedidoProductos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pedidos.Models
+{
+    public class P_PedidoProductos
+    {
+        public int id { get; set; }
+        public int idPedido { get; set; }
+        public int idProducto { get; set; }
+        public decimal valorProducto { get; set; }
+    }
+}
---
+++ Pedidos/SqlScripts/PedidoProdutos.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE P_PedidoProductos(
+    id int not null identity(1,1),   
+    idPedido int not null,   
+    idProducto int not null,
+    valorProducto decimal(18,5) null, 
+  );
+    
+        

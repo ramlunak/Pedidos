86d6f7a6501181cce820ff21385da73676bcfe7f
Royber <royber@yardim.com.br>
2020-11-22 12:09:28 -0300

    keyup input

---
+++ Pedidos/wwwroot/js/views/Pedidos/keyup.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+
+    $(document).keyup(function (e) {
+
+        //inputNome ENTER
+        if ($("#inputNome").is(":focus") && (e.keyCode == 13)) {
+            $("#inputTelefone").focus();
+        }
+        else if ($("#inputTelefone").is(":focus") && (e.keyCode == 13)) {
+                $("#inputEndereco").focus();
+        }
+        else if ($("#inputEndereco").is(":focus") && (e.keyCode == 13)) {
+            $("#inputProducto").focus();
+        }
+        
+    });
+
+});

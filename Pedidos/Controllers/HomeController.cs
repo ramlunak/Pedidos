efb311f515aa2eda6e82e88f0a3f5d413d7b721e
Royber <royber2020@gmail.com>
2020-12-14 22:29:52 -0300

    migrando net core de 5.0.1 a 3.1.8

--- Pedidos/Controllers/HomeController.cs
+++ Pedidos/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using AspNetCore.Reporting;
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;

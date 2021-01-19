8d596c13091bbb6ffc409082314dd69d56337798
Royber <royber@yardim.com.br>
2021-01-19 20:56:33 -0300

    cargar cliente por id

--- Pedidos/Hubs/CardapioHub.cs
+++ Pedidos/Hubs/CardapioHub.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using Pedidos.Data;
+using Pedidos.Extensions;
 using Pedidos.Models;
 using System;
 using System.Collections.Generic;
@@ -10,12 +13,19 @@ namespace Pedidos.Hubs
 {
     public class CardapioHub : Hub
     {
+        private readonly AppDbContext _context;
+
+        public CardapioHub(AppDbContext context)
+        {
+            _context = context;
+        }
 
         public string ServerGetConnectionId() => Context.ConnectionId;
 
         public async Task ServerAbrirMesa(string idCuenta, string valor)
         {
-            await Clients.All.SendAsync("clientAbrirMesa", idCuenta, valor);
+            var clientes = await _context.P_Clientes.Where(x => x.idCuenta.ToString() == idCuenta).ToListAsync();
+            await Clients.All.SendAsync("clientAbrirMesa", idCuenta, clientes.ToJson());
         }
 
 

a31a1f1ba84f0ca16324bdb87001e6ec7c0851cb
Royber <royber2020@gmail.com>
2021-01-22 22:58:15 -0300

    06

--- Pedidos/Hangfire/Jobs.cs
+++ Pedidos/Hangfire/Jobs.cs
@@ -1,32 +1,72 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using Pedidos.Controllers;
 using Pedidos.Data;
 using Pedidos.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Pedidos.Hangfire
 {
     public class Jobs : AppDbContext
     {
+
         public Jobs()
         {
 
         }
 
+        static async Task<List<P_Cuenta>> GetCuentas()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync("https://devmastereat.azurewebsites.net/api/cuentas");
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<List<P_Cuenta>>(responseBody);
+                }
+                catch (HttpRequestException e)
+                {
+                    return default(List<P_Cuenta>);
+                }
+            }
+        }
+
+        static async Task<List<P_Cuenta>> GetPedidosByIdCuenta()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync("https://devmastereat.azurewebsites.net/api/cuentas");
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<List<P_Cuenta>>(responseBody);
+                }
+                catch (HttpRequestException e)
+                {
+                    return default(List<P_Cuenta>);
+                }
+            }
+        }
+
         public async Task RelatorioVendasSaidasAnual()
         {
-            try
-            {
-                // var cuentas = await _context.P_Cuentas.ToListAsync();
-            }
-            catch (Exception ex)
-            {
-                ;
-            }
+            var cuentas = await GetCuentas();
 
+            if (cuentas != null)
+                foreach (var item in cuentas)
+                {
+
+
+
+
+                }
             ;
         }
     }
---
+++ Pedidos/api/PedidosController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pedidos.Data;
+using Pedidos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Pedidos.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PedidosController : ControllerBase
+    {
+
+        private readonly AppDbContext _context;
+
+        public PedidosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<P_Pedido>> Get()
+        {
+            return await _context.P_Pedidos.ToListAsync();
+        }
+
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<AccountController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<AccountController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AccountController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

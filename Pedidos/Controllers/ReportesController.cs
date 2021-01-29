5e303f7adddbb5620c87792f3b15a8b2707c1fb0
Royber <royber@yardim.com.br>
2021-01-29 05:48:42 -0300

    order by cantidad

--- Pedidos/Controllers/ReportesController.cs
+++ Pedidos/Controllers/ReportesController.cs
@@ -191,76 +191,8 @@ namespace Pedidos.Controllers
             {
                 return RedirectToAction("Salir", "Login");
             }
-
-            try
-            {
-                ViewBag.HayVentas = false;
-                var fecha = DateTime.Now.ToSouthAmericaStandard();
-                var pedidos = await _context.P_Pedidos.Where(x => x.idCuenta == Cuenta.id && x.jsonFormaPagamento != null && x.status == StatusPedido.Finalizado.ToString() && x.fecha == fecha).ToListAsync();
-                var caja = new P_Caja();
-
-                if (pedidos.Count > 0)
-                {
-                    ViewBag.HayVentas = true;
-
-                    var listaProdustos = new List<P_Productos>();
-
-                    foreach (var item in pedidos)
-                    {
-                        listaProdustos.AddRange(item.jsonListProductos.ConvertTo<List<P_Productos>>());
-                    }
-
-                    var grupo = listaProdustos.GroupBy(x => x.id).ToList();
-
-                    var ventasProductoPorPeriodo = new List<VentasProductoPorPeriodo>();
-
-                    foreach (var g in grupo)
-                    {
-                        var productoPorPeriodo = new VentasProductoPorPeriodo();
-                        productoPorPeriodo.id = g.Key;
-                        productoPorPeriodo.nombre = g.ToList().FirstOrDefault().nombre;
-                        productoPorPeriodo.count = g.ToList().Sum(x => x.cantidad);
-                    }
-
-                    var formasPagamento = await _context.P_FormaPagamento.Where(x => x.idCuenta == Cuenta.id).ToListAsync();
-
-                    foreach (var pedido in pedidos)
-                    {
-
-                        pedido.listaFormaPagamento = pedido.jsonFormaPagamento.ConvertTo<List<P_FormaPagamento>>().Where(x => x.valor.HasValue).OrderBy(x => x.nombre).ToList();
-
-                        foreach (var item in pedido.listaFormaPagamento)
-                        {
-                            formasPagamento.Where(x => x.id == item.id).ToList().ForEach(x => x.valor += item.valor.Value);
-                            if (item.tasa.HasValue)
-                            {
-                                formasPagamento.Where(x => x.id == item.id).ToList().ForEach(x => x.valorTasa += item.valorTasa);
-                            }
-                        }
-                    }
-
-
-                    caja.idCuenta = Cuenta.id;
-                    caja.idUltimoPedido = pedidos.OrderBy(x => x.id).LastOrDefault().id;
-                    caja.fecha = DateTime.Now;
-                    caja.totalVentas = pedidos.Sum(x => x.valorProductos);
-                    caja.totalDescuentos = pedidos.Sum(x => x.descuento);
-                    caja.totalTasasEntrega = pedidos.Sum(x => x.tasaEntrega);
-                    caja.totalTasas = pedidos.Sum(x => x.listaFormaPagamento.Sum(f => f.valorTasa));
-                    caja.formaPagamentos = formasPagamento.OrderBy(x => x.nombre).Where(x => x.valor > 0).ToList();
-                    caja.jsonFormaPagamento = caja.formaPagamentos.ToJson();
-
-                }
-
-                return View(caja);
-            }
-            catch (Exception ex)
-            {
-                ViewBag.Erro = true;
-                await InsertLog(_context, Cuenta.id, ex.ToString());
-                PrompErro(ex.Message);
-                return View();
-            }
+            ViewBag.HayVentas = false;
+            return View();
         }
 
         [HttpPost]
@@ -336,7 +268,7 @@ namespace Pedidos.Controllers
                     ventasProductoPorPeriodo.Add(productoPorPeriodo);
                 }
 
-                ViewBag.VentasProductoPorPeriodo = ventasProductoPorPeriodo;
+                ViewBag.VentasProductoPorPeriodo = ventasProductoPorPeriodo.OrderByDescending(x => x.count);
 
                 if (ventasProductoPorPeriodo != null && ventasProductoPorPeriodo.Any())
                     ViewBag.HayVentas = true;

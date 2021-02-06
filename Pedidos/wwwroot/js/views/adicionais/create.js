f1c57206565a3e27da83219a3291cdddc86c8834
Royber <royber@yardim.com.br>
2021-02-05 22:35:38 -0300

    mask monkey

--- Pedidos/wwwroot/js/views/adicionais/create.js
+++ Pedidos/wwwroot/js/views/adicionais/create.js
@@ -1,23 +1,8 @@
 ﻿
+
 $(function () {
-    $("#txtValor").mask("###0.00", { reverse: true });
-  
-    //$("#inputPrecioRecarga").on("input", (e) => {
 
-    //    var value = parseFloat($("#inputPrecioRecarga").val());
-
-    //    //Calcular el % para una recarga con valor de 20 USD
-    //    if (!isNaN(value)) {
-    //        var porciento = value * 100 / 20;
-    //        $("#InputShowPorcientoCalculado").val(porciento.toFixed(2).replace(',','.'));
-    //        $("#inputPorciento").val(porciento.toFixed(2).replace(',', '.'));
-    //    } else {
-    //        $("#InputShowPorcientoCalculado").val(null);
-    //        $("#inputPrecioRecarga").val(null);
-    //    }
-      
-
-    //});
+    $('#txtValor').maskMoney({ prefix: '', allowNegative: false, thousands: '', decimal: '.', affixesStay: false });
 
 });
 

07688e5ecf1f3db9273cf6fdc624607851fb6e42
Royber <royber2020@gmail.com>
2021-02-16 12:37:31 -0300

    ajustes imprimir

--- Pedidos/wwwroot/js/views/Pedidos/MostrarPedidosPendientes.js
+++ Pedidos/wwwroot/js/views/Pedidos/MostrarPedidosPendientes.js
@@ -366,7 +366,7 @@ function imprimirPedido(idPedido, pendientes) {
             comprobantePedido.append('<div class="centrado mb-1"> <b>VIAGEM</b> </div >');
         }
 
-        if (pedido.direccion === null && pedido.direccion === "" && pedido.idMesa === null) {
+        if ((pedido.direccion === null || pedido.direccion === "") && pedido.idMesa === null) {
             comprobantePedido.append('<div class="centrado mb-1"> <b>RETIRADA</b> </div >');
         }
 

e17e4028222932f976b9e7384a5aaef327b1fd43
Royber <royber@yardim.com.br>
2021-02-17 06:24:29 -0300

    ajustes barrios

--- Pedidos/Models/P_Barrio.cs
+++ Pedidos/Models/P_Barrio.cs
@@ -1,6 +1,7 @@
 ﻿using Pedidos.Models.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,11 +11,14 @@ namespace Pedidos.Models
     public class P_Barrio
     {
         public int id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "O Estado é obrigatorio")]
+        [DisplayName("Estado")]
         public string estado { get; set; }
-        [Required]
+        [Required(ErrorMessage = "O Municipio é obrigatorio")]
+        [DisplayName("Municipio")]
         public string municipio { get; set; }
-        [Required]
+        [Required(ErrorMessage = "O Barrio é obrigatorio")]
+        [DisplayName("Nome")]
         public string nombre { get; set; }
         public decimal? tasa { get; set; }
         public int idCuenta { get; set; }

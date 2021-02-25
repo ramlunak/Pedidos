75a7efdb185fc298f4a0f520d7db1ee320a977ca
Royber <royber@yardim.com.br>
2021-02-24 22:43:51 -0300

    vue pack

---
+++ Pedidos/wwwroot/js/views/IntegracionPedidos/vue_index.js
@@ -0,0 +1,15 @@
+﻿
+var content = new Vue({
+    el: '#content',
+    created: function () {
+        console.log($('#btnAdd'));
+    },
+    data: {
+        message: 'Hello Vue!',
+        barrios: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' }
+        ]
+    }
+})

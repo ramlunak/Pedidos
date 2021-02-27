b248486d8cd6e0fe073c3a8c2561208b38ce37a9
Royber <royber@yardim.com.br>
2021-02-26 21:07:58 -0300

    rutas

--- Pedidos/Controllers/IntegracionPedidosController.cs
+++ Pedidos/Controllers/IntegracionPedidosController.cs
@@ -63,5 +63,34 @@ namespace Pedidos.Controllers
         }
 
 
+        public async Task<IActionResult> GetRutas()
+        {
+            if (!ValidarCuenta())
+            {
+                return RedirectToAction("Salir", "Login");
+            }
+
+            try
+            {
+                var integracionesPedido = await _context.P_IntegracionPedidos.Where(x => x.idCuentaIntegracion == Cuenta.id && x.statusIntegracion == StatusIntegracionPedido.Esperando.ToString()).ToListAsync();
+                var grupoPedidosPorBarrio = from integracion in integracionesPedido
+                                            group integracion by integracion.barrio.ToUpper() into g
+                                            select new DTOGrupoPedidosPorBarrio
+                                            {
+                                                barrio = g.Key,
+                                                idBarrio = g.First().idBarrio,
+                                                count = g.Count(),
+                                                listIntegracionPedidos = g.ToList()
+                                            };
+
+                return Ok(grupoPedidosPorBarrio);
+            }
+            catch (Exception ex)
+            {
+                return NotFound();
+            }
+        }
+
+
     }
 }
--- Pedidos/wwwroot/js/views/IntegracionPedidos/vue_index.js
+++ Pedidos/wwwroot/js/views/IntegracionPedidos/vue_index.js
@@ -1,56 +1,70 @@
 ﻿var app = new Vue({
     el: '#content',
     created: function () {
-
-        $.ajax({
-            type: "GET",
-            url: "/IntegracionPedidos/GetGruposPedidoPorBarrio/",
-            traditional: true,
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            success: function (data) {
-                app.grupoPedidosPorBarrio = data;
-                console.log(data);
-                // this.isLoading = false;
-            },
-            failure: function (response) {
-                console.log('failure', response);
-                // this.isLoading = false;
-            },
-            error: function (response) {
-                console.log('error', response);
-                //this.isLoading = false;
-            }
-        });
+        GetGruposPedidoPorBarrio();
     },
     data: {
         isLoading: false,
-        message: 'Hello Vue!',
-        grupoPedidosPorBarrio: []
-    },    
+        grupoPedidosPorBarrio: [],
+        rutas: []
+    },
     methods: {
         CargarGruposPedidos: function () {
-
-            $.ajax({
-                type: "GET",
-                url: "/IntegracionPedidos/GetGruposPedidoPorBarrio/",
-                traditional: true,
-                contentType: "application/json; charset=utf-8",
-                dataType: "json",
-                success: function (data) {
-                    app.grupoPedidosPorBarrio = data;
-                    // this.isLoading = false;
-                },
-                failure: function (response) {
-                    console.log('failure', response);
-                    // this.isLoading = false;
-                },
-                error: function (response) {
-                    console.log('error', response);
-                    //this.isLoading = false;
-                }
-            });
-
+            GetGruposPedidoPorBarrio();
         },
     }
-})
+});
+
+
+function GetGruposPedidoPorBarrio() {
+
+    $.ajax({
+        type: "GET",
+        url: "/IntegracionPedidos/GetGruposPedidoPorBarrio/",
+        traditional: true,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+            app.grupoPedidosPorBarrio = data;
+            console.log(data);
+            // this.isLoading = false;
+        },
+        failure: function (response) {
+            console.log('failure', response);
+            // this.isLoading = false;
+        },
+        error: function (response) {
+            console.log('error', response);
+            //this.isLoading = false;
+        }
+    });
+}
+
+function GetRutas() {
+
+    $.ajax({
+        type: "GET",
+        url: "/IntegracionPedidos/GetRutas/",
+        traditional: true,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+            app.rutas = data;
+            console.log(data);
+            // this.isLoading = false;
+        },
+        failure: function (response) {
+            console.log('failure', response);
+            // this.isLoading = false;
+        },
+        error: function (response) {
+            console.log('error', response);
+            //this.isLoading = false;
+        }
+    });
+
+}
+
+function CrearNuevaRuta() {
+
+}

3d4ba5fbcd1dfd2812d62bb92e1b0739083150f9
Royber <royber2020@gmail.com>
2021-03-07 09:45:08 -0300

    03

---
+++ Pedidos/Controllers/DeliveryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pedidos.Controllers
+{
+    public class DeliveryController : Controller
+    {
+        public IActionResult Index(string link)
+        {
+            ViewBag.Link = link;
+            return View();
+        }
+    }
+}

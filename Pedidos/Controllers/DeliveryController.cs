258193b5fd8f2ed0c78da82b39ca384a16c447f5
Royber <royber@yardim.com.br>
2021-03-07 11:21:42 -0300

    03

--- Pedidos/Controllers/DeliveryController.cs
+++ Pedidos/Controllers/DeliveryController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pedidos.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,12 +8,39 @@ using System.Threading.Tasks;
 
 namespace Pedidos.Controllers
 {
-    public class DeliveryController : Controller
+    public class DeliveryController : BaseController
     {
-        public IActionResult Index(string link)
+        private readonly AppDbContext _context;
+
+        public DeliveryController(AppDbContext context)
         {
-            ViewBag.Link = link;
+            _context = context;
+        }
+
+        [Route("delivery/{id?}")]
+        public IActionResult Index(string id)
+        {
+            ViewBag.Link = id;
             return View();
         }
+         
+        public async Task<IActionResult> CargarDatos()
+        {
+            try
+            {
+                var productos = await _context.P_Productos.FromSqlRaw(SqlConsultas.GetSqlProductosAll(5)).ToListAsync();
+                SetSession("CardapioProductos", productos);
+
+                var categorias = await _context.P_Categorias.Where(x => x.idCuenta == 5 && x.activo).ToListAsync();
+                SetSession("CardapioCategorias", productos);
+
+                return Ok(categorias);
+            }
+            catch (Exception ex)
+            {
+                return NotFound();
+            }
+        }
+
     }
 }
